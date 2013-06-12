0bd74925c282919221bb0d93fd869249c06bc1a5
Jamil Abreu <jamil@noobninja.com>
2013-06-12 13:28:44 -0400

    noobninja

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Noobninja::Application.routes.draw do
   root :to => 'users#welcome'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :users
 
   # The priority is based upon order of creation:
