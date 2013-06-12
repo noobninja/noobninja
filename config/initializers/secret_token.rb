c164d10eb42d2d27a6aeec63e738df2d4d33b284
Jamil Abreu <jamil@noobninja.com>
2013-06-11 23:53:59 -0400

    noobninja

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noobninja::Application.config.secret_token = '695ba0fc4e2851d780bf592718c1f38513d9fff32f16f89d9d5f33969b833a5521f4c79acb4f42be3924988e5be3ac0396b1cf1386de509f440b40f4ddb9a83c'
