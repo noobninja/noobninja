113dc6e2fd0c3bde154fe7f5229aca6e7b9f90b3
Jamil Abreu <abreu.jamil@gmail.com>
2013-06-27 16:43:23 -0400

    donations fix

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,6 +1,6 @@
 class DonationsController < ApplicationController
   def index
     @donations = Donation.where(counted: true)
-    @total = @donations ? @donations.map(&:amount).inject(:+) / 100 : 0
+    @total = @donations.present? ? (@donations.map(&:amount).inject(:+) / 100) : 0
   end
 end
